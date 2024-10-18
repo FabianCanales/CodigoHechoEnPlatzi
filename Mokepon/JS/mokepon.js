97d6693f6ff4140567277a94407ebfe7da96e344
FabianCanales <fabian.canales.salazar@gmail.com>
2024-10-17 22:31:28 -0300

    "Agregado logica para que PC eliga MOkepon al azar "

--- Mokepon/JS/mokepon.js
+++ Mokepon/JS/mokepon.js
@@ -4,12 +4,35 @@ function iniciarJuego() {
 }
 
 function seleccionarMascotaJugador() {
-  if (document.getElementById("hipodoge").checked == true) {
-    alert("Seleccionaste Como Mascota A HIPODOGE ");
-  } else if (document.getElementById("capipepo").checked == true) {
-    alert("Seleccionaste Como Mascota A CAPIPEPO");
-  } else if (document.getElementById("ratigueya").checked == true) {
-    alert("Has Seleccionado Como  Mascota A RATIGUEYA ");
+  let inputHipodoge = document.getElementById("hipodoge");
+  let inputCapipepo = document.getElementById("capipepo");
+  let inputRatigueya = document.getElementById("ratigueya");
+  let spanMascotaJugador = document.getElementById("Mascota-Jugador");
+
+  if (inputHipodoge.checked == true) {
+    spanMascotaJugador.innerHTML = "Hipodoge";
+  } else if (inputCapipepo.checked == true) {
+    spanMascotaJugador.innerHTML = "Capipepo";
+  } else if (inputRatigueya.checked == true) {
+    spanMascotaJugador.innerHTML = "Ratigueya";
+  } else {
+    alert("No has seleccionado una Mascota \n Por favor selecciona una ");
   }
+
+  seleccionarMascotaEnemigo();
+}
+function seleccionarMascotaEnemigo() {
+  let spanMascotaEnemigo = document.getElementById("Mascota-Enemigo");
+  let AtaqueAleatorio = aleatorio(1, 3);
+  if (AtaqueAleatorio == 1) {
+    spanMascotaEnemigo.innerHTML = "Hipodoge";
+  } else if (AtaqueAleatorio == 2) {
+    spanMascotaEnemigo.innerHTML = "Capipepo";
+  } else if (AtaqueAleatorio == 3) {
+    spanMascotaEnemigo.innerHTML = "Ratigueya";
+  }
+}
+function aleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 window.addEventListener("load", iniciarJuego);
